94eba5f6f674e4dbb6679c3c2dd77694c8e6a31e
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2019-02-07 21:29:06 +0200

    Fix dotenv import

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
